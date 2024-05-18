d6dfb0b3262687669c714a92d2fbe853343b0a23
Travis McCawley <travis.r.mccawley@gmail.com>
2024-05-18 11:38:25 -0700

    make config an abstract class and add policy based retirement costs

--- fire_sim.py
+++ fire_sim.py
@@ -1,95 +1,77 @@
-from constants import END_AGE, NUM_ITERATIONS, INFLATION_RATE, SOCIAL_SECURITY_AGE
+from constants import END_AGE, NUM_ITERATIONS, SOCIAL_SECURITY_AGE, INFLATION_RATE
 from market_model import get_random_market_return_rate
 from config import Config
 import matplotlib.pyplot as plt
+import numpy as np
 
 class SimState:
     def __init__(
             self,
-            start_age:int,
-            start_balance_millions:float,
-            retirement_costs:float,
-            retirement_costs_age: int,
-            social_security_monthly: float,
-            plot_flag: bool,
+            config: Config,
         ):
-        # constants
-        self.start_age = start_age
-        self.start_balance_millions = start_balance_millions
-        self.retirement_costs = retirement_costs
-        self.retirement_costs_age = retirement_costs_age
-        self.social_security_monthly = social_security_monthly
-        self.plot_flag = plot_flag
-        # iteration variables
-        self.it_age = None
-        self.it_balance = None
-        self.it_costs = None
-        self.it_age_array = []
-        self.it_balance_array = []
+        # config (inputs)
+        self.config = config
+        # initiailize iteration variables
+        self.start_interation()
         # outputs
-        self.it_end_balance = []
-        if self.plot_flag:
+        self.it_end_balance_array = []
+        self.it_age_zero_balance_array = []
+        if self.config.get_plot_flag():
             plt.figure()
 
     def start_interation(self):
-        self.it_age = self.start_age
-        self.it_balance = self.start_balance_millions*1E6
+        self.it_age = self.config.get_retirement_start_age()
+        self.it_age_zero_balance = END_AGE
+        self.it_prev_market_return_rate = 0.0
+        self.it_balance = self.config.get_retirement_start_balance()
         self.it_age_array = []
         self.it_balance_array = []
     
     def step_iteration(self):
         market_return_rate = get_random_market_return_rate()
-        costs = self.get_retirement_costs(self.it_age)
-        social_security = self.get_social_security_annual(self.it_age)
-        market_return = (self.it_balance - costs/2) * market_return_rate
+        costs = self.config.get_retirement_costs_inflation_adjusted(self.it_age,self.it_prev_market_return_rate)
+        social_security = self.config.get_social_security_annual_inflation_adjusted(self.it_age)
+        market_return = (self.it_balance - costs/2) * market_return_rate # average market balance
         self.it_balance += market_return + social_security - costs
+        # save outputs
+        if self.it_balance <= 0 and self.it_age < self.it_age_zero_balance:
+            self.it_age_zero_balance = self.it_age
         # increment
         self.it_age += 1
-        if self.plot_flag:
+        self.it_prev_market_return_rate = market_return
+        if self.config.get_plot_flag():
             self.it_age_array.append(self.it_age)
             self.it_balance_array.append(self.it_balance)
     
     def end_iteration(self):
-        self.it_end_balance.append(self.it_balance)
+        self.it_end_balance_array.append(self.it_balance)
+        self.it_age_zero_balance_array.append(self.it_age_zero_balance)
         plt.plot(self.it_age_array,self.it_balance_array)
 
-    def get_retirement_costs(self,age):
-        if age < self.retirement_costs_age:
-            raise Exception(f"{age} less than {self.retirement_costs_age}")
-        return(self.retirement_costs*(1 + INFLATION_RATE)**(age - self.retirement_costs_age))
-
-    def get_social_security_annual(self,age):
-        return(self.social_security_monthly*12 if age >= SOCIAL_SECURITY_AGE else 0)
-
     def get_risk(self):
-        return(len(list(filter(lambda x: x < 0,self.it_end_balance)))/len(self.it_end_balance))
+        return(len(list(filter(lambda x: x <= 0,self.it_end_balance_array)))/len(self.it_end_balance_array))
+    
+    def get_average_zero_balance_age(self):
+        return(np.average(self.it_age_zero_balance_array))
     
     def results(self):
         print("===INPUTS===")
-        print(f"  Retirement Age: {self.start_age}")
-        print(f"  Starting Balance: ${self.start_balance_millions}M")
-        print(f"  Annual Retirement Costs @ Age {self.retirement_costs_age}: ${round(self.retirement_costs)}")
+        print(f"  Retirement Age: {self.config.get_retirement_start_age()}")
+        print(f"  Starting Balance: ${self.config.get_retirement_start_balance_m()}M")
         print(f"  Inflation: {round(INFLATION_RATE*100,2)}%")
         print(f"  N Iterations: {NUM_ITERATIONS}")
         print("===OUTPUTS===")
         print(f"  Risk: {int(round(self.get_risk()*100,0))}%")
-        if self.plot_flag:
+        print(f"  Average Age of Zero Balance: {int(round(self.get_average_zero_balance_age(),0))}")
+        if self.config.get_plot_flag():
             plt.title("FIRE Sims")
             plt.grid()
             plt.xlabel("Age [yr]")
             plt.ylabel("Balance [$]")
-            # plt.ylim(0,9E6)
             plt.show()
     
-
-sim_state = SimState(
-    Config.start_age,
-    Config.start_balance_millions,
-    Config.retirement_costs,
-    Config.retirement_costs_age,
-    Config.social_security_monthly,
-    Config.plot_flag,
-)
+config = Config()
+sim_state = SimState(config)
 for iteration in range(NUM_ITERATIONS):
     sim_state.start_interation()
     while sim_state.it_age < END_AGE:
---
+++ utils.py
@@ -0,0 +1,59 @@
+from constants import INFLATION_RATE, SOCIAL_SECURITY_AGE
+from abc import ABC, abstractmethod
+
+def inflation_adjustment(initial_amount: float,time_period: float) -> float:
+    if time_period < 0:
+        raise Exception(f"{time_period} less than 0")
+    return(initial_amount*(1 + INFLATION_RATE)**time_period)
+
+class ConfigABC(ABC):
+    @abstractmethod
+    def get_retirement_start_age(self) -> int:
+        """Returns the age at start of retirement"""
+        pass
+
+    @abstractmethod
+    def get_retirement_start_balance_m(self) -> float:
+        """Returns the account balance (in millions of dollars) at start of retirement"""
+        pass
+
+    @abstractmethod
+    def get_initial_social_secuirty_monthly(self) -> float:
+        """Returns the initial social security amount (once social security begins)"""
+        pass
+
+    @abstractmethod
+    def get_plot_flag(self) -> bool:
+        """Returns a boolean indicating if visualization plots will be generated from the sim"""
+        pass
+
+    @abstractmethod
+    def get_baseline_retirement_costs_age(self) -> int:
+        """Returns an age for which the retirement costs are baselined for inflation (inflation adjustments will be computed from this age)"""
+        pass
+
+    @abstractmethod
+    def get_baseline_retirement_costs(self,previous_year_market_return_rate:float) -> float:
+        """Returns the annual retirement costs for a given baseline (get_baseline_retirement_costs_age)"""
+        pass
+
+    def get_retirement_start_balance(self) -> float:
+        """Returns the starting retirmenet balance"""
+        return(self.get_retirement_start_balance_m()*1E6)
+    
+    def get_retirement_costs_inflation_adjusted(self,age:int,previous_year_market_return_rate:float) -> float:
+        """Returns the annual retirement costs after inflation adjustment"""
+        if age < self.get_baseline_retirement_costs_age():
+            raise Exception(f"{age} less than {self.get_baseline_retirement_costs_age()}")
+        initial_amount = self.get_baseline_retirement_costs(previous_year_market_return_rate)
+        time_period = age - self.get_baseline_retirement_costs_age()
+        return(inflation_adjustment(initial_amount,time_period))
+    
+    def get_social_security_annual_inflation_adjusted(self,age:int) -> float:
+        """Returns the annual social security benefit after inflation adjustment"""
+        if age < SOCIAL_SECURITY_AGE:
+            # No SS benefit yet
+            return(0)
+        initial_amount = 12.0*self.get_initial_social_secuirty_monthly()
+        time_period = age - SOCIAL_SECURITY_AGE
+        return(inflation_adjustment(initial_amount,time_period))
