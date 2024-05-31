a62f42812b5885ba24e09035cacf72dc2c81da12
Travis McCawley <travis.r.mccawley@gmail.com>
2024-05-30 20:11:15 -0700

    stuff

--- constants.py
+++ constants.py
@@ -3,6 +3,6 @@ import numpy as np
 NUM_ITERATIONS = 10000
 END_AGE = 90
 RNG = np.random.default_rng(None)
-INFLATION_RATE = 0.0253
+INFLATION_RATE = 0.0253 # 30 year average
 SOCIAL_SECURITY_AGE = 62
-BONDS_AVERAGE_RETURNS_RATE = 0.04
+BONDS_AVERAGE_RETURNS_RATE = 0.0425 # 30 year average for BND
